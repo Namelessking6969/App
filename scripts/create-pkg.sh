bd57e16b987cb8b55f7db3222f61bc4b73ca7be1
NamelessKing6969 <herbabyboii666@gmail.com>
2026-05-12 11:03:22 -0400

    bump version to 3.0.13

--- scripts/create-pkg.sh
+++ scripts/create-pkg.sh
@@ -23,7 +23,7 @@ mkdir -p "$PKG_OUTPUT"
 
 # Create product metadata
 PRODUCT_NAME="$PROJECT_NAME"
-PRODUCT_VERSION="3.0.12"
+PRODUCT_VERSION="3.0.13"
 IDENTIFIER="com.vibeterminal.app"
 
 # Create PKG using productbuild
